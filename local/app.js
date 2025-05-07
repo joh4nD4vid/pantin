5ae7e12e9f1e84fc73874ad0c0dee4ede44e3f04
Johan David <johan.david@free.fr>
2025-05-07 17:51:07 +0200

    Avancées squelette

--- local/app.js
+++ local/app.js
@@ -1,110 +1,269 @@
-'use strict';
+// Events généraux
+// - - - - - - - - - - - - - - - - - - - - - - 
 
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import Stats from 'three/examples/jsm/libs/stats.module';
-import * as dat from 'dat.gui';
+    document.addEventListener('click', (e)=>{
 
+        if ( picture.hover ) { 
+          new Skeleton_Point( e.offsetX, e.offsetY ); 
+        }
 
-let vertexShader = null;
-let fragmentShader = null;
-let material = null;
-let time = 0.0;
-
-let speed = {
-  value : 1.0
-};
-
-// Variables GUI
-const gui = new dat.GUI();
-gui.add(speed, 'value', 0, 2, 0.1);
-
-
-// Shader uniforms 
-let uniforms = {
-
-  u_time : {
-      type : 'f',
-      value : time
-  },
-
-  u_resolution : {
-    type : "v2",
-    value : new THREE.Vector2(window.innerWidth, window.innerHeight)
-        .multiplyScalar(window.devicePixelRatio)
-  },
-
-  u_speed : {
-    type : "f",
-    value : speed
-  }
-
-};
-
-
-
-// Chargement dynamique des shaders
-let request_vertex = new XMLHttpRequest();
-let request_fragment = new XMLHttpRequest();
-
-// On les récupère sous forme de texte, à la bonne adresse
-request_vertex.open('GET', 'shaders/vertex_shader.glsl', true);
-request_vertex.responseType = 'text';
-
-request_fragment.open('GET', 'shaders/fragment_shader.glsl', true);
-request_fragment.responseType = 'text';
-
-
-// Chaque chargement effectué appelle la fonction correspondante
-request_vertex.onload = () => {
-
-    if ( request_vertex.readyState === request_vertex.DONE && request_vertex.status === 200 ){
-        vertexDone( request_vertex.responseText );
-    }
-}
-
-request_fragment.onload = () => {
-    if ( request_fragment.readyState === request_fragment.DONE && request_fragment.status === 200 ){
-        fragmentDone( request_fragment.responseText );
-    }
-}
-
-request_vertex.send(null);
-request_fragment.send(null);
-
-
-// Ces fonctions vérifient si l'autre shader est chargé.
-// Si c'est le cas, on appelle shadersDone()
-function vertexDone( text ) {
-    vertexShader = text;
-    if (fragmentShader !== null) {
-        shadersDone();
-    }
-}
-
-function fragmentDone( text ) {
-    fragmentShader = text;
-    if (vertexShader !== null) {
-        shadersDone();
-    }
-}
-
-// On peut créer notre matériau et ursuivre.
-function shadersDone() {
-
-    // Create the shader material
-    material = new THREE.MeshLambertMaterial({ color : 0xFFFF00 });
-
-
-    material = new THREE.ShaderMaterial({
-        uniforms : uniforms,
-        vertexShader : vertexShader,
-        fragmentShader : fragmentShader
     });
 
-    // start scene
-    init();
-    animate();
+// - - - - - - - - - - - - - - - - - - - - - - 
+
+
+
+
+
+
+
+
+const drop = {
+
+
+    zone : document.getElementById('dropZone'),
+
+
+    events : () => {
+
+
+        drop.zone.addEventListener('dragover', (event) => {
+
+          event.preventDefault();
+          dropZone.style.borderColor = '#000';
+
+        });
+
+
+        drop.zone.addEventListener('dragleave', () => {
+          dropZone.style.borderColor = '#ccc';
+        });
+
+
+        drop.zone.addEventListener('drop', (event) => {
+
+          event.preventDefault();
+          dropZone.style.borderColor = '#ccc';
+
+          const files = event.dataTransfer.files;
+
+          if (files.length > 0) {
+
+            drop.end();
+            picture.new( files[0] );
+
+          }
+
+      });
+
+
+    },
+
+
+    end : () => { drop.zone.remove(); }
+
+
+};
+
+
+drop.events();
+
+
+
+
+
+
+
+
+const picture = {
+
+
+
+    dom : undefined,
+    wrap : undefined,
+    axeX : document.querySelector('#axeX'),
+    axeY : document.querySelector('#axeY'),
+
+    hover : false,
+
+
+
+    new : ( file ) => {
+
+        if (file) {
+
+            const wrap = picture.wrap();
+
+            const img = document.createElement('img');
+            const wrap_img = document.createElement('div');
+            const reader = new FileReader();
+
+            reader.onload = (e) => {
+
+              img.onload = () => {
+
+                console.log( img.naturalWidth );
+                wrap_img.style.width = img.naturalWidth + 'px';
+                wrap_img.style.height = img.naturalHeight + 'px';
+                wrap_img.id =  "wrap_img";
+
+
+                img.width = img.naturalWidth;
+                img.height = img.naturalHeight;
+                img.id = "picture";
+                wrap_img.appendChild(img);
+                wrap.append( wrap_img );
+
+
+                picture.dom = document.getElementById('picture');
+                picture.wrap = document.getElementById('wrap_img');
+                picture.events();
+
+              };
+
+              img.src = e.target.result;
+
+            };
+
+            reader.readAsDataURL(file);
+
+        }
+
+    },
+
+    
+
+    wrap : () => {
+
+        let wrap = document.createElement('div');
+        wrap.id = "wrap-picture";
+        document.body.append( wrap );
+
+        return document.getElementById('wrap-picture');
+
+    },
+
+
+
+    events : () => {
+
+
+        picture.wrap.addEventListener('mousemove', (e)=>{
+
+            picture.display_axes( true );
+            picture.update_axes_pos( e );
+            picture.hover = true;
+
+        });
+
+
+        picture.wrap.addEventListener('mouseleave', (e)=>{
+
+           picture.display_axes( false );
+           picture.hover = false;
+
+        });
+
+    },
+
+
+
+    display_axes : ( action ) => {
+
+        let className = action ? '' : 'invisible';
+        picture.axeX.className = className;
+        picture.axeY.className = className;
+
+    },
+
+
+
+    update_axes_pos : (e) => {
+
+        picture.axeX.style.left = e.clientX + 'px'; 
+        picture.axeY.style.top = e.clientY + 'px'; 
+
+    }
+
+
+
+};
+
+
+
+
+
+
+
+class Skeleton_Point {
+
+
+    constructor( x, y ) {
+
+        this.prec_id = 'SK_' + Skeleton_Point.id;
+
+        Skeleton_Point.id++;
+
+        this.id = 'SK_' + Skeleton_Point.id;
+        this.x = x;
+        this.y = y;
+
+        Skeleton_Point.all[ this.id ] = this;
+        this.init();
+        this.testLink();
+
+    }
+
+
+    static id = 0;
+    static HALF_POINT_SIZE = 7;
+
+    static all = {};
+
+
+    static get_by_id( id ) {
+
+        if ( Skeleton_Point.all[ id ] ) {
+            return Skeleton_Point.all[ id ];
+        }
+        return false;
+
+    }
+
+
+
+    init() {
+        this.div = document.createElement('div');
+        this.div.className = 'skeleton-point';
+        this.div.id = this.id;
+        
+        this.pos();
+        this.insert();
+    }
+
+
+    pos() {
+        this.div.style.top = ( this.y - Skeleton_Point.HALF_POINT_SIZE ) + 'px';
+        this.div.style.left = ( this.x - Skeleton_Point.HALF_POINT_SIZE ) + 'px';
+    }
+
+
+    insert() {
+        picture.wrap.append( this.div );
+        this.ref = document.querySelector('#' + this.id );
+    }
+
+
+    testLink() {
+
+        const a = Skeleton_Point.get_by_id( this.prec_id );
+
+        if ( a ) {
+            new Link( a, this );
+        };
+
+    }
+
 
 
 }
@@ -114,87 +273,38 @@ function shadersDone() {
 
 
 
-var camera, scene, renderer, light, monCube, stats;
-var controls;
 
 
-function init() {
+class Link {
 
-  // Scene
-  const canvas = document.querySelector('#canvas');
+    constructor( a, b ) {
+        Link.id++;
 
-  renderer = new THREE.WebGLRenderer({ 
-    canvas : canvas, 
-    antialias : true
-  });
-
-  scene = new THREE.Scene();
-
-  renderer.setClearColor(0xf0f0f0);
-  renderer.setPixelRatio( window.devicePixelRatio );
-  renderer.setSize( window.innerWidth, window.innerHeight );
-  document.body.appendChild( renderer.domElement );
-
-  window.addEventListener('resize', onWindowResize, false);
+        this.a = a;
+        this.b = b;
+        this.id = 'link_' + Link.id;
+        Link.all[ this.id ] = this;
+    }
 
 
-
-  // Camera.
-  const fov = 45;
-  const aspect = window.innerWidth / window.innerHeight;
-  const near = 0.1;
-  const far = 2000;
-  camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-  camera.position.z = 30;
+    static CLASSNAME = "link";
+    static id = 0;
+    static all = {};
 
 
-  // Orbit controls.
-  controls = new OrbitControls(camera, renderer.domElement);
-  controls.enablePan = true;
-  controls.enableKeys = false;
-  controls.update();
-  camera.controls = controls;
+    draw() {
+        let div = document.createElement('div');
+        div.id = this.id;
+        div.className = Link.CLASSNAME;
+        
+    }
 
-  
-  //Stats
-  stats = new Stats();
-  document.body.appendChild(stats.dom);
-
-  // Cube
-  var geometry = new THREE.BoxGeometry( 5, 5, 5 );
-  monCube = new THREE.Mesh(geometry, material );
-  scene.add(monCube);
-
-  // Lights.
-  light = new THREE.DirectionalLight(0xffffff, 3.0);
-  light.position.set(0, 11, 0);
-  scene.add(light);
 
 }
 
 
-// Un autre exemple : avec une boucle de rendu
-function animate() {
-  requestAnimationFrame( animate );
-  render();
-}
-
-function render() {
-  time++;
-
-  monCube.rotation.x += (0.03 * speed.value );
-  monCube.rotation.y += (0.05 * speed.value );
-  monCube.rotation.z += (0.07 * speed.value );
-
-  stats.update();
-  renderer.render( scene, camera );
-}
 
 
 
-function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize( window.innerWidth, window.innerHeight );
-}
+
 
